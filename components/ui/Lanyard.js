81dc161a2e11069b5bc7bacb45794f69870c3ec9
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-21 07:11:58 +0700

    update code

--- components/ui/Lanyard.js
+++ components/ui/Lanyard.js
@@ -152,12 +152,14 @@ function Band({
   const j3 = useRef(null);
   const card = useRef(null);
 
-  const { vec, ang, rot, dir } = useMemo(
+  const { vec, ang, rot, dir, tmpA, tmpB } = useMemo(
     () => ({
       vec: new THREE.Vector3(),
       ang: new THREE.Vector3(),
       rot: new THREE.Vector3(),
       dir: new THREE.Vector3(),
+      tmpA: new THREE.Vector3(),
+      tmpB: new THREE.Vector3(),
     }),
     []
   );
