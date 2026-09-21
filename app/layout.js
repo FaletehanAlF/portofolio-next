f93305e7ea733dc06312fb6db4d1204439a3acec
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-21 10:06:26 +0700

    update code

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import ClickSpark from "@/components/ui/ClickSpark.js";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,7 +20,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col bg-[#0a0a0a] text-zinc-100">{children}</body>
+      <body className="min-h-full flex flex-col bg-[#0a0a0a] text-zinc-100">
+        <ClickSpark
+          sparkColor="#fff"
+          sparkSize={10}
+          sparkRadius={18}
+          sparkCount={8}
+          duration={400}
+          className="flex min-h-screen flex-col"
+        >
+          {children}
+        </ClickSpark>
+      </body>
     </html>
   );
 }
